0931b949abd5632247e8824581cc2795033c6903
AbdulRehmanHashmi <155216102+AbdulRehmanHashmi@users.noreply.github.com>
2024-03-02 03:56:13 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,62 @@
+var num1 = 55
+var num2 = 30
+
+// Addition
+var Add = num1 + num2
+console.log(Add)
+
+
+// Subtraction
+var Subtr = num1 - num2
+console.log(Subtr)
+
+// Multiplication
+var Multiply = num1 * num2
+console.log(Multiply)
+
+// Division
+var Divide = num1 / num2
+console.log(Divide)
+
+// Modulus
+var Remainder = num1 % 4
+console.log(Remainder)
+
+// Exponentiation
+var Power = num2 ** 2
+console.log(Power)
+
+var Power1 = num1 ** 2
+console.log(Power1)
+
+// Marksheet
+var MathsTotal = 100
+var MathsObtain = 80
+
+var UrduTotal = 100
+var UrduObtain = 78
+
+var EnglishTotal = 100
+var EnglishObtain = 84
+
+var ChemistryTotal = 75
+var ChemistryObtain = 42
+
+var PhysicsTotal = 75
+var PhysicsObtain = 45
+
+// Calculating Total And Obtain
+var AllTotal = MathsTotal + UrduTotal + EnglishTotal + ChemistryTotal + PhysicsTotal
+var AllObtain = MathsObtain + UrduObtain + EnglishObtain + ChemistryObtain + PhysicsObtain
+console.log(AllTotal)
+console.log(AllObtain)
+
+// Calculating Percentage
+var Percentage = AllObtain * 100 / AllTotal
+console.log(Percentage)
+
+
+
+
+
+
